990b86f7951d3a0f99337467fda6dc2c54e9d2ba
FUQQZe11 <zelenin.aleks2520@gmail.com>
2022-02-16 00:01:42 +0500

    Добавлен фильтр Собеля

--- finder.py
+++ finder.py
@@ -1,7 +1,7 @@
 import cv2
-import sys
 import numpy as np
 from matplotlib import pyplot as plt
+import skimage.exposure as exposure
 
 
 '''
@@ -22,23 +22,42 @@ from matplotlib import pyplot as plt
 
 '''            
 
-class processed_image():
-    def __init__(self):
+class processedImage():
+    def __init__(self, image):
         self.blur = cv2.bilateralFilter(image, 9, 150, 150)
+        self.gray = cv2.cvtColor(self.blur, cv2.COLOR_BGR2GRAY)
 
-    def sobel_func(self, image):
-        pass
+    def sobel_func(self):
+        # apply sobel derivatives
+        sobelx = cv2.Sobel(self.blur,cv2.CV_64F,1,0,ksize=3)
+        sobely = cv2.Sobel(self.blur,cv2.CV_64F,0,1,ksize=3)
+        # square 
+        sobelx2 = cv2.multiply(sobelx,sobelx)
+        sobely2 = cv2.multiply(sobely,sobely)
+        # add together and take square root
+        sobel_magnitude = cv2.sqrt(sobelx2 + sobely2)
+        # normalize to range 0 to 255 and clip negatives
+        self.sobel_magnitude = exposure.rescale_intensity(sobel_magnitude, in_range='image', out_range=(0,255)).clip(0,255).astype(np.uint8)
+        return self.sobel_magnitude
 
-    def find_contours(self):
-        pass
+    def find_contours(self, filtered_image):
+        self.contours, self.hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
+        return self.contours
+
+    def show_image(self):
+        cv2.imshow('window', self.blur)
+        cv2.waitKey(0)
 
 
+    
 
-def draw_contours():
-    pass
+def draw_contours(image, contours):
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
 
 
 if __name__ == "__main__":
     img = cv2.imread('paint0.jpeg')
-    cv2.imshow('window', gaussian_func(img))
+    my_image = processedImage(img)
+    #draw_contours(my_image.blur, my_image.find_contours(my_image.sobel_func()))
+    cv2.imshow('window', my_image.sobel_func())
     cv2.waitKey(0)
