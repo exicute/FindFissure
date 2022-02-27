566dab68c76f33120e6aac823504beb5b54b3a4f
exicute <secret.exicute@gmail.com>
2022-02-27 22:31:42 +0500

    сделал грубый поиск контуров

--- finder.py
+++ finder.py
@@ -4,60 +4,45 @@ from matplotlib import pyplot as plt
 import skimage.exposure as exposure
 
 
-'''
-Две части разработки:
-    1) определить hsv параметр для трещины(возможно перебрав все пиксели, можно найти резкий перепад, сравнив между собой несколько соседних)
-    2) преобразовать картинку в найденный hsv и разбив его на куски с помощью shape, для каждого куска строить find contours, либо просто использовать findcontours
-
-Первый пунк является основным, т к. позволяет определить цвет трещины, что нам и надо
-
-Разбить картинку на квадратике, просматривать какждый и смотреть сколько в нем цветов, если цветов будет несколько значит в этом квадрате есть трещина
-
-Характеристика поиска трещины:
-    1) вокруг трещины имеется контур значение value(v из HSV) которого сильно меньше чем в самой трещине
-    2) цвет внутри трещины очень яркий(нужно узнать на сколько)
-    3) трещина имеет характерную форму
-
-!!!Решение: просто сгладить картинку(размытие по Гауссу) -> затем применить фильтр Собеля -> найти контуры
-
-'''            
-
 class processedImage():
     def __init__(self, image):
         self.blur = cv2.bilateralFilter(image, 9, 150, 150)
         self.gray = cv2.cvtColor(self.blur, cv2.COLOR_BGR2GRAY)
 
-    def sobel_func(self):
         # apply sobel derivatives
-        sobelx = cv2.Sobel(self.blur,cv2.CV_64F,1,0,ksize=3)
-        sobely = cv2.Sobel(self.blur,cv2.CV_64F,0,1,ksize=3)
+        sobelx = cv2.Sobel(self.gray,cv2.CV_64F,1,0,ksize=3)
+        sobely = cv2.Sobel(self.gray,cv2.CV_64F,0,1,ksize=3)
         # square 
         sobelx2 = cv2.multiply(sobelx,sobelx)
         sobely2 = cv2.multiply(sobely,sobely)
         # add together and take square root
         sobel_magnitude = cv2.sqrt(sobelx2 + sobely2)
         # normalize to range 0 to 255 and clip negatives
-        self.sobel_magnitude = exposure.rescale_intensity(sobel_magnitude, in_range='image', out_range=(0,255)).clip(0,255).astype(np.uint8)
-        return self.sobel_magnitude
+        self.sobel_magnitude = exposure.rescale_intensity(sobel_magnitude, in_range='image', out_range=(0,255)).clip(70,255).astype(np.uint8)
+        ret, self.thresh = cv2.threshold(self.sobel_magnitude, 128, 255, cv2.THRESH_BINARY)
 
-    def find_contours(self, filtered_image):
-        self.contours, self.hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
-        return self.contours
+        kernel = np.ones((8,8), np.uint8)
+        self.opening = cv2.morphologyEx(self.thresh, cv2.MORPH_CLOSE, kernel)
+
+        self.contours, self.hierarchy = cv2.findContours(self.opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
 
     def show_image(self):
         cv2.imshow('window', self.blur)
         cv2.waitKey(0)
 
 
+def delete_defects(item, image):
+    for i in range(len(item.contours)-1, 0, -1):
+        if cv2.contourArea(item.contours[i])<50:
+            continue
+        else:
+            cv2.drawContours(image, item.contours[i], -1, (0, 255, 0), 2)
+
     
 
-def draw_contours(image, contours):
-    cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
-
-
 if __name__ == "__main__":
-    img = cv2.imread('paint0.jpeg')
+    img = cv2.imread('paint3.jpeg')
     my_image = processedImage(img)
-    #draw_contours(my_image.blur, my_image.find_contours(my_image.sobel_func()))
-    cv2.imshow('window', my_image.sobel_func())
+    delete_defects(my_image, img)
+    cv2.imshow('window', img)
     cv2.waitKey(0)
